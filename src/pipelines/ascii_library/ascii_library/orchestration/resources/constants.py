7fddda6101ca9dade24bfa99b01975b522bb65fb
CI Hotfix <cihotfix@ascii.ac.at>
2025-04-05 21:21:11 +0000

    move files from ascii_library_demo into base src and sync from research-space to main branch of ascii-hydra

---
+++ src/pipelines/ascii_library/ascii_library/orchestration/resources/constants.py
@@ -0,0 +1,10 @@
+
+instance_profile = "INTSANCE_PROFILE"
+s3_log_uri = "S3_LOG_URI"
+master_security_group = "MASTER_SECURITY_GROUP"
+slave_security_group = "SLAVE_SECURITY_GROUO"
+service_role = "SERVICE_ROLE"
+job_role = "JOB_ROLE"
+aws_region = "AWS_REGION"
+rackspace_user = "RACKSPACE_USER"
+
